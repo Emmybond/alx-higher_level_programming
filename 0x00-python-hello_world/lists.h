4b06c4679f42fe34e7c57613673edc487676863c
Emmanuel <emmanuelademuyiwa007@gmail.com>
2023-06-05 15:13:17 -0700

    0. Run Python file

---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,27 @@
+#ifndef lists_h
+#define lists_h
+
+#include <stdlib.h>
+
+/**
+ * struct listint_s - It represents singly linked list
+ * @n: It represents integer
+ * @next: It points to the next node
+ *
+ * Description: This describes singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
+
+#endif /* lists_h */
+
+
